7a1a3f8cf47afad0584c13d7718824d865198265
Harrison Heeb <harrisonheeb@Harrisons-MBP-2.hitronhub.home>
2021-04-24 21:19:41 -0500

    add timer

--- udp-server.c
+++ udp-server.c
@@ -83,7 +83,7 @@ tcpip_handler(void)
         printf("FAILURE!\n");
     }
 
-    printf("decrypted message: '%s'", (char*)in)
+    printf("decrypted message: '%s'", (char*)in);
 
 
     PRINTF("\n");
