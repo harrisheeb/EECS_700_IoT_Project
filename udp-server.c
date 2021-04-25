bee17e260b3e36427bd84063b364e23f5527691f
Harrison Heeb <harrisonheeb@Harrisons-MBP-2.hitronhub.home>
2021-04-24 22:12:00 -0500

    add timer

--- udp-server.c
+++ udp-server.c
@@ -87,7 +87,7 @@ tcpip_handler(void)
     int i;
     char* m_string;
     m_string = (char*)in;
-    for(i = 16; i < 38; i++){
+    for(i = 16; i < 41; i++){
       m_string[i] = ' ';
     }
     printf("%s", m_string);
